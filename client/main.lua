e059a6a15fa8514d5a05ab8c3c2554b4f58e7fb4
Kovalskis <81760561+aquapha@users.noreply.github.com>
2021-06-28 02:53:45 +0300

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -58,11 +58,14 @@ end)
 
 function comma_value(amount)
   local formatted = amount
-  while true do  
+  
+  while true do
     formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
-    if (k==0) then
+    
+    if k == 0 then
       break
     end
   end
+  
   return formatted
 end
